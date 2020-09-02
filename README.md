7c798f5c3096341cb2264fa1cd2af363d88d068d
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-09-02 23:20:53 +0900

    pre

---
+++ README.md
@@ -0,0 +1,30 @@
+# Music Box
+
+## install
+
+### python3 virtualenv
+
+```bash
+$ cd
+$ python3 -m venv env1
+$ cd env1
+$ . bin/activate
+```
+
+### clone git repository
+
+```bash
+$ cd ~/env1
+
+$ git clone git@github.com:ytani01/music-box.git
+or 
+$ git clone https://github.com/ytani01/music-box.git
+```
+
+### setup.sh
+
+```bash
+$ cd ~/env1/music-box
+$ . ../bin/activate
+$ ./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -ux
+#
+# (c) 2020 Yoichi Tanibayashi
+#
+
+##############################################################################
+MYNAME=`basename $0`
+MYDIR=`dirname $0`
+echo "MYNAME=$MYDIR"
+cd $MYDIR
+echo "===" `pwd`
+
+
+##############################################################################
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "$MYNAME: Please activate venv"
+    exit 1
+fi
+VENVNAME=`basename $VIRTUAL_ENV`
+echo "VENVNAME=$VENVNAME"
+
+##############################################################################
+# update pip
+echo "=== update pip command"
+set -e
+pip install -U pip
+hash -r
+pip -V
+set +e
+
+##############################################################################
+# install python packages
+echo "=== install python packages"
+set -e
+pip install -r requirements.txt
+set +e
+
+##############################################################################
