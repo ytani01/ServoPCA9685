8dd92ca047fbcebfdc16ca62d12b3349a9331f4e
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2025-05-17 17:31:35 +0900

    FIX

--- servoPCA9685/servo.py
+++ servoPCA9685/servo.py
@@ -23,7 +23,7 @@ class Servo:
     PW_MAX = 2500
     PW_CENTER = int((PW_MIN + PW_MAX) / 2)
 
-    def __init__(self, channel=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+    def __init__(self, channel=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
                  pi=None, debug=False):
         """ Constructor
 
